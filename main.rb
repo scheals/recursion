93ea19d83cbaa34d6f21f5a8503b61c697fda785
scheals <piecoo@gmail.com>
2022-03-01 07:13:08 +0100

    Introduce hacky solution to fibs_rec

--- main.rb
+++ main.rb
@@ -1,20 +1,22 @@
 def fibs(number)
   all_the_fibs = []
-  for i in 0...number
-    case i
-    when 0
-      all_the_fibs << 0
-    when 1
-      all_the_fibs << 1
-    else
-      all_the_fibs << all_the_fibs[i - 1] + all_the_fibs[i - 2]
-    end
+  (0...number).each do |num|
+    all_the_fibs << case num
+                    when 0
+                      0
+                    when 1
+                      1
+                    else
+                      all_the_fibs[num - 1] + all_the_fibs[num - 2]
+                    end
   end
   all_the_fibs
 end
 
 def fibs_rec(number, all_the_fibs = [])
-  return all_the_fibs if number < 0
+  number -= 1 if all_the_fibs.empty?
+  return all_the_fibs.reverse if number.negative?
+
   all_the_fibs << recursive_fibonacci(number)
   fibs_rec(number - 1, all_the_fibs)
 end
